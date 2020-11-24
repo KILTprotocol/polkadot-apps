3637682ed66e73a1b758e793418b3c7e7b782ff1
Albrecht <14820950+weichweich@users.noreply.github.com>
2020-11-24 08:33:34 +0100

    some cleanup

--- packages/page-js/src/snippets/polimec-demo.ts
+++ packages/page-js/src/snippets/polimec-demo.ts
@@ -10,65 +10,78 @@ export const polimecDemo: Snippet = {
   value: 'polimecDemo',
   text: '(Polimec) Make transfer and listen to events',
   label: { color: 'red', children: 'Extrinsics', size: 'tiny' },
-  code: `const ALICE = '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY'
-  const BOB = '5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty'
-  const TEST_CURRENCY = '0x0000000000000000'
-
-  console.log('BALANCES:')
-  console.log(
-    'Alice:',
-    (await api.query.poliBalances.accounts(ALICE, TEST_CURRENCY)).free,
-  )
-  console.log(
-    'Bob:',
-    (await api.query.poliBalances.accounts(BOB, TEST_CURRENCY)).free,
-  )
-
-  // Get a random number between 1 and 100000
-  const randomAmount = Math.floor(Math.random() * 100000 + 1)
-
-  // Create a extrinsic, transferring randomAmount units to Bob.
-  const transfer = api.tx.preCurrencyMint.transfer(
-    TEST_CURRENCY,
-    ALICE,
-    randomAmount,
-  )
-
-  // Sign and Send the transaction
-  await new Promise((resolve) => {
-    transfer.signAndSend(BOB, ({ events = [], status }) => {
+  code: `// Helper for waiting until transaction is in block
+const sendAndAwaitInBlock = async (tx) => {
+  return new Promise((resolve) => {
+    tx.send(({ events = [], status }) => {
       if (status.isInBlock) {
         console.log(
-          'Successful transfer of ' +
+          "Successful transfer of " +
             randomAmount +
-            ' with hash ' +
-            status.asInBlock.toHex(),
-        )
-        events.forEach(({ phase, event: { data, method, section } }) => {
-          console.log(
-            phase.toString() +
-              ' : ' +
-              section +
-              '.' +
-              method +
-              ' ' +
-              data.toString(),
-          )
-        })
-          resolve()
+            " with hash " +
+            status.asInBlock.toHex()
+        );
+        resolve();
       } else {
-        console.log('Status of transfer: ' + status.type)
+        console.log("Status of transfer: " + status.type);
       }
-    })
-  })
 
-  console.log('BALANCES:')
-  console.log(
-    'Alice:',
-    (await api.query.poliBalances.accounts(ALICE, TEST_CURRENCY)).free,
-  )
-  console.log(
-    'Bob:',
-    (await api.query.poliBalances.accounts(BOB, TEST_CURRENCY)).free,
-  )`
+      events.forEach(({ phase, event: { data, method, section } }) => {
+        console.log(
+          phase.toString() +
+            " : " +
+            section +
+            "." +
+            method +
+            " " +
+            data.toString()
+        );
+      });
+    });
+  });
+};
+
+// #############################################################################
+// Start of DEMO:
+
+// addresses of BOB and Alice, ID of currency that should be transferred
+const ALICE = "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY";
+const BOB = "5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty";
+const TEST_CURRENCY = "0x0000000000000000";
+
+// Print balances
+console.log("BALANCES:");
+console.log(
+  "Alice:",
+  (await api.query.poliBalances.accounts(ALICE, TEST_CURRENCY)).free
+);
+console.log(
+  "Bob:",
+  (await api.query.poliBalances.accounts(BOB, TEST_CURRENCY)).free
+);
+
+// Get a random number between 1 and 100000
+const randomAmount = Math.floor(Math.random() * 100000 + 1);
+
+// Create a extrinsic, transferring randomAmount units to Alice.
+const transfer = api.tx.preCurrencyMint.transfer(
+  TEST_CURRENCY,
+  ALICE,
+  randomAmount
+);
+
+// Sign transaction and send it
+await sendAndAwaitInBlock(await transfer.signAsync(BOB));
+
+// print balances again
+console.log("BALANCES:");
+console.log(
+  "Alice:",
+  (await api.query.poliBalances.accounts(ALICE, TEST_CURRENCY)).free
+);
+console.log(
+  "Bob:",
+  (await api.query.poliBalances.accounts(BOB, TEST_CURRENCY)).free
+);
+`
 };
